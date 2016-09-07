f3104f7c79ab047e6a893cb8f92bbf910f444e05
lkoller <lukaskoller6@gmail.com>
2016-09-06 22:29:42 -0700

    cleaning up project in git

--- ToDoList/app/src/main/java/com/koller/lukas/todolist/ColorPickerDialogCallback.java
+++
@@ -1,8 +0,0 @@
-package com.koller.lukas.todolist;
-
-/**
- * Created by Lukas on 13.06.2016.
- */
-public interface ColorPickerDialogCallback {
-    public void colorPicked(int red, int green, int blue, int textColor);
-}
--- ToDoList/app/src/main/java/com/koller/lukas/todolist/RetrievedDataFromAppFolderCallback.java
+++
@@ -1,8 +0,0 @@
-package com.koller.lukas.todolist;
-
-/**
- * Created by Lukas on 13.06.2016.
- */
-public interface RetrievedDataFromAppFolderCallback {
-    public void retrievedDataFromAppFolder(String data);
-}
